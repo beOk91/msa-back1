c2140e0402845e6b55264d1c1fbaa231dbbb3655
beOk91 <be_ok91@naver.com>
2022-12-04 21:35:28 +0900

    first ms commit

---
+++ src/main/java/com/msa/cj/api/controller/MyController1.java
@@ -0,0 +1,19 @@
+package com.msa.cj.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/first")
+@Slf4j
+public class MyController1 {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello first service";
+	}
+
+}
